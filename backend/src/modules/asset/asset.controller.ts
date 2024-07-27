70d7450d2aef5a8c7d950b299e6efb71c00858e0
glob <glob@pop-os.localdomain>
2024-07-27 18:24:27 -0300

    back:auth-roles

--- backend/src/modules/asset/asset.controller.ts
+++ backend/src/modules/asset/asset.controller.ts
@@ -1,34 +1,54 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { AssetService } from './asset.service';
 import { CreateAssetDto } from './dto/create-asset.dto';
 import { AssetDto } from './dto/asset.dto';
+import { User } from '../user/model/user.entity';
+import { LoggedUser } from 'src/decorators/user.decorator';
+import { JwtAuthGuard } from '../auth/guard/jwt.guard';
+import { UserRole } from '../user/enum/user-role.enum';
 
 @Controller('assets')
+@UseGuards(JwtAuthGuard)
 export class AssetController {
   constructor(private readonly assetService: AssetService) {}
 
   @Post()
-  async create(@Body() body: CreateAssetDto) {
-    const asset = await this.assetService.create(body);
-
-    return new AssetDto(asset);
+  async create(@LoggedUser() loggedUser: User, @Body() body: CreateAssetDto) {
+    if (loggedUser.role === UserRole.Admin) {
+      const asset = await this.assetService.create(body);
+      return new AssetDto(asset);
+    }
   }
 
   @Get()
-  async findAll() {
-    const assets = await this.assetService.findAll();
-    return assets.map((u) => new AssetDto(u));
+  async findAll(@LoggedUser() loggedUser: User) {
+    if (loggedUser.role === UserRole.Admin) {
+      const assets = await this.assetService.findAll();
+      return assets.map((u) => new AssetDto(u));
+    }
   }
 
   @Get(':id')
-  async findById(@Param('id') id: string) {
-    const asset = await this.assetService.findById(id);
-    return new AssetDto(asset);
+  async findById(@LoggedUser() loggedUser: User, @Param('id') id: string) {
+    if (loggedUser.role === UserRole.Admin) {
+      const asset = await this.assetService.findById(id);
+      return new AssetDto(asset);
+    }
   }
 
   @Delete(':id')
-  async deleteById(@Param('id') id: string) {
-    const asset = await this.assetService.deleteById(id);
-    return new AssetDto(asset);
+  async deleteById(@LoggedUser() loggedUser: User, @Param('id') id: string) {
+    if (loggedUser.role === UserRole.Admin) {
+      const asset = await this.assetService.deleteById(id);
+      return new AssetDto(asset);
+    }
   }
 }
--- backend/src/modules/purchase/purchase.controller.ts
+++ backend/src/modules/purchase/purchase.controller.ts
@@ -1,9 +1,22 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { PurchaseService } from './purchase.service';
 import { CreatePurchaseDto } from './dto/create-purchase.dto';
 import { PurchaseDto } from './dto/purchase.dto';
+import { JwtAuthGuard } from '../auth/guard/jwt.guard';
+import { LoggedUser } from 'src/decorators/user.decorator';
+import { User } from '../user/model/user.entity';
+import { UserRole } from '../user/enum/user-role.enum';
 
 @Controller('purchases')
+@UseGuards(JwtAuthGuard)
 export class PurchaseController {
   constructor(private readonly purchaseService: PurchaseService) {}
 
@@ -15,20 +28,26 @@ export class PurchaseController {
   }
 
   @Get()
-  async findAll() {
-    const purchases = await this.purchaseService.findAll();
-    return purchases.map((u) => new PurchaseDto(u));
+  async findAll(@LoggedUser() loggedUser: User) {
+    if (loggedUser.role === UserRole.Admin) {
+      const purchases = await this.purchaseService.findAll();
+      return purchases.map((u) => new PurchaseDto(u));
+    }
   }
 
-  @Get(':id')
-  async findById(@Param('id') id: string) {
-    const purchase = await this.purchaseService.findById(id);
-    return new PurchaseDto(purchase);
+  @Get('/:id')
+  async findById(@LoggedUser() loggedUser: User, @Param('id') id: string) {
+    if (loggedUser.role === UserRole.Admin) {
+      const purchase = await this.purchaseService.findById(id);
+      return new PurchaseDto(purchase);
+    }
   }
 
-  @Delete(':id')
-  async deleteById(@Param('id') id: string) {
-    const purchase = await this.purchaseService.deleteById(id);
-    return new PurchaseDto(purchase);
+  @Delete('/:id')
+  async deleteById(@LoggedUser() loggedUser: User, @Param('id') id: string) {
+    if (loggedUser.role === UserRole.Admin) {
+      const purchase = await this.purchaseService.deleteById(id);
+      return new PurchaseDto(purchase);
+    }
   }
 }
